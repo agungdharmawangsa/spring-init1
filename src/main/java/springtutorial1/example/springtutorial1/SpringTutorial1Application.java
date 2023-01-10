a0422e3b08a6941a719cbad4a6c596f1fd6d2b18
agunggeekseat <agung.dharmawangsa@geekseat.com.au>
2023-01-10 16:50:23 +0800

    clean up swagger config

--- src/main/java/springtutorial1/example/springtutorial1/SpringTutorial1Application.java
+++ src/main/java/springtutorial1/example/springtutorial1/SpringTutorial1Application.java
@@ -4,11 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-@EnableWebMvc
 public class SpringTutorial1Application {
 
 	public static void main(String[] args) {
@@ -16,12 +13,7 @@ public class SpringTutorial1Application {
 	}
 
 	@Bean
-	public ModelMapper modelMapper(){
+	public ModelMapper modelMapper() {
 		return new ModelMapper();
-	}
-
-	@Bean
-	public InternalResourceViewResolver defaultViewResolver() {
-		return new InternalResourceViewResolver();
 	}
 }
